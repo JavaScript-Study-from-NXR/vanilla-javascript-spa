064bd98db36b6b450d1d51042f61daaea969781e
hyewonny2327 <wonny2327@gmail.com>
2026-01-07 15:15:04 +0900

    feat : 장바구니 상품 개수 업데이트

--- src/events/counterEvents.js
+++ src/events/counterEvents.js
@@ -10,16 +10,21 @@ export function initCounterEvents() {
 
 function handleDecrease(btn) {
   const cartItem = btn.closest(".cart-item");
+  const productId = cartItem.dataset.productId;
   const detailCounter = btn.closest(".product-detail-counter");
 
   // cart 페이지
   if (cartItem) {
     const input = cartItem.querySelector("#quantity-input");
     const value = Number(input.value);
+    const nextValue = value - 1;
+    input.value = nextValue;
 
     if (value > 1) input.value = value - 1;
 
-    // cartStore.updateQuantity(...)
+    //로컬 스토리지에서 개수 수정
+    updateCartQuantity(productId, nextValue);
+
     return;
   }
 
@@ -35,16 +40,19 @@ function handleDecrease(btn) {
 
 function handleIncrease(btn) {
   const cartItem = btn.closest(".cart-item");
+  const productId = cartItem.dataset.productId;
+
   const detailCounter = btn.closest(".product-detail-counter");
 
   // cart 페이지
   if (cartItem) {
     const input = cartItem.querySelector("#quantity-input");
     const value = Number(input.value);
+    const nextValue = value + 1;
+    input.value = nextValue;
 
-    input.value = value + 1;
-
-    // cartStore.updateQuantity(...)
+    //로컬 스토리지에서 개수 수정
+    updateCartQuantity(productId, nextValue);
     return;
   }
 
@@ -56,4 +64,19 @@ function handleIncrease(btn) {
     input.value = value + 1;
     return;
   }
+}
+
+function updateCartQuantity(productId, nextQuantity) {
+  console.log("ddd");
+  const raw = localStorage.getItem("cart");
+  if (!raw) return;
+
+  const cart = JSON.parse(raw);
+
+  const target = cart.find((item) => item.productId === productId);
+  if (!target) return;
+
+  target.quantity = nextQuantity;
+
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
